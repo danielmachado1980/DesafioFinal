ae437b6536bba5e105aaf0678f9679e11f0d99f8
Daniel Machado <daniel.g_machado@hotmail.com>
2019-10-18 17:38:28 -0300

    Teste Redis

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,18 @@
 import app from './app';
+import redisConfig from './config/redis';
 
-app.listen(3333);
+const redis = require('redis');
+
+const redisClient = redis.createClient({ host: redisConfig.host, port: redisConfig.port });
+
+redisClient.on('ready', () => {
+  console.log('Redis is ready');
+});
+
+redisClient.on('error', () => {
+  console.log('Error in Redis');
+});
+
+app.listen(3333, () => {
+  console.log('Listening on Port 3333...');
+});
